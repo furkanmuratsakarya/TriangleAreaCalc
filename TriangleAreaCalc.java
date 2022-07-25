d5685213e8a0227b0c0e18aac07f21204336a5df
Furkan Murat SAKARYA <sakarya.furkanm@gmail.com>
2022-07-25 22:50:51 +0300

    .

---
+++ TriangleAreaCalc.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        int edgeA, edgeB, edgeC, halfPerimeter;
+        double area;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first edge:\t");
+        edgeA = input.nextInt();
+        System.out.println("Enter second edge:\t");
+        edgeB = input.nextInt();
+        System.out.println("Enter third edge:\t");
+        edgeC = input.nextInt();
+
+        halfPerimeter = (edgeA + edgeB + edgeC) / 2;
+        area = Math.sqrt[halfPerimeter * (halfPerimeter - edgeA) * (halfPerimeter - edgeB) * (halfPerimeter - edgeC)];
+
+         System.out.println("Area of the triangle: " + area);       
+
+    }
+}
